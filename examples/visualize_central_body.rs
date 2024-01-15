1fd1ed5f73a257e4a113f0d3d6f42d0938977cf3
ChHecker <39056704+ChHecker@users.noreply.github.com>
2024-01-15 22:36:38 +0100

    Add central body example

---
+++ examples/visualize_central_body.rs
@@ -0,0 +1,20 @@
+use barnes_hut::{
+    gravity::GravitationalAcceleration, particle_creator::CentralBodyParticleCreator,
+    visualization::Visualizer,
+};
+use rand_distr::{Normal, Uniform};
+
+const SPEED: f32 = 1.;
+
+fn main() {
+    // Generate random particles.
+    let uniform_mass = Uniform::new(0., 1e4);
+    let normal_rad = Normal::new(2., 0.2).unwrap();
+
+    let acc = GravitationalAcceleration::new(1e-6);
+    let pc = CentralBodyParticleCreator::new(1e10, uniform_mass, normal_rad);
+
+    // Visualize.
+    let vis = Visualizer::from_particle_creator(pc, 2, acc, 1920, 1080).unwrap();
+    vis.visualize(SPEED, 1.5).unwrap();
+}
---
+++ examples/visualize_random.rs
@@ -0,0 +1,23 @@
+use barnes_hut::{
+    gravity::{GravitationalAcceleration, GravitationalParticle},
+    particle_creator::DistrParticleCreator,
+    visualization::Visualizer,
+};
+use rand_distr::{Normal, Uniform};
+
+const SPEED: f32 = 100.;
+
+fn main() {
+    // Generate random masses, positions, and velocities.
+    let uniform_mass = Uniform::new(0., 1e4);
+    let normal_pos = Normal::new(0., 3.).unwrap();
+    let normal_vel = Normal::new(0., 0.05 / SPEED).unwrap();
+
+    let acc = GravitationalAcceleration::new(1e-2);
+    let pc = DistrParticleCreator::new(uniform_mass, uniform_mass, normal_pos, normal_vel);
+
+    // Visualize.
+    let vis: Visualizer<f32, GravitationalParticle<f32>> =
+        Visualizer::from_particle_creator(pc, 100, acc, 1920, 1080).unwrap();
+    vis.visualize(SPEED, 1.5).unwrap();
+}
