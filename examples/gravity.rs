9b406264606d455508b2dacce54d5d2a92843da3
ChHecker <39056704+ChHecker@users.noreply.github.com>
2026-03-27 11:09:55 +0100

    Fix example

--- examples/gravity.rs
+++ examples/gravity.rs
@@ -1,22 +1,17 @@
-use barnes_hut::{
-    Particles, Simulation, Sorting, Step, barnes_hut::BarnesHutSimd, particles::PosStorage,
-};
+use barnes_hut::{Particles, Simulation, Sorting, Step, barnes_hut::BarnesHutSimd};
+
 use nalgebra::Vector3;
 use rand::{Rng, SeedableRng, rngs::StdRng};
 
 fn main() {
     let mut rng = StdRng::seed_from_u64(0);
 
-    let num_pars = 100_000;
+    let num_pars = 1_000_000;
     let particles = (0..num_pars)
         .map(|_| {
             (
                 rng.random_range(0.0..1000.0),
-                Vector3::new(
-                    PosStorage(rng.random()),
-                    PosStorage(rng.random()),
-                    PosStorage(rng.random()),
-                ),
+                Vector3::new_random(),
                 10f32 * Vector3::new_random(),
             )
         })
@@ -28,14 +23,10 @@ fn main() {
 
     let mut acceleration = vec![Vector3::zeros(); num_pars];
 
-    let num_steps = 1_000;
+    let num_steps = 10;
     for t in 0..num_steps {
         let current_step = Step::from_index(t, num_steps, Sorting::EveryNIteration(100));
-
-        if t % 100 == 0 {
-            println!("{t} out of {num_steps} time steps done.");
-        }
-
+        println!("{t} out of {num_steps} time steps done.");
         bh.step(&mut acceleration, 0.1, current_step);
     }
 }
